edb4fb3ae994353c7194508ff1e9310e57892a34
KorchaginS <KorchaginSV1995@yandex.ru>
2023-10-28 23:15:41 +0300

    Написал юнит тесты

---
+++ books/store/tests/test_serializer.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from store.models import Book
+from store.serializers import BooksSerializer
+
+
+class BookSerializerTestCase(TestCase):
+
+    def test_serialization(self):
+        # Сначала сериализируем объекты тестируемым сериализатором
+        book_1 = Book.objects.create(name='Test book 1', price='25')
+        book_2 = Book.objects.create(name='Test book 2', price='75')
+
+        serializer_data = BooksSerializer([book_1, book_2], many=True).data
+
+        # Потом собираем ожидаемый результат в коде руками
+        expected_data = [
+            {
+                'id': book_1.id,
+                'name':'Test book 1',
+                'price':'25.00'
+            },
+            {
+                'id': book_2.id,
+                'name': 'Test book 2',
+                'price': '75.00'
+            }
+        ]
+        # Сравниваем
+        self.assertEqual(serializer_data, expected_data)
+
